9f020954faec19bebca08b62a61bb2f512eb3839
Andrei Gatej <andreigtj01@gmail.com>
2023-01-09 00:34:17 +0200

    feat(student): mock api when fetching a class' grades

--- client/src/api/student.ts
+++ client/src/api/student.ts
@@ -4,6 +4,17 @@ export interface SchoolClass {
   studentsCount: number;
 }
 
+export interface ClassGrade {
+  id: number;
+  value: number;
+  assignedAt: string;
+}
+
+export interface SelectedClass {
+  teacherName: string;
+  grades: ClassGrade[];
+}
+
 export const fetchClasses = (): Promise<SchoolClass[]> => {
   const classes = [
     { id: 1, subjectName: 'Subject1', studentsCount: 10 },
@@ -15,3 +26,18 @@ export const fetchClasses = (): Promise<SchoolClass[]> => {
 
   return Promise.resolve(classes);
 };
+
+export const fetchClassGrades = (classId: number): Promise<SelectedClass> => {
+  const grades: ClassGrade[] = [
+    { id: 1, value: 8, assignedAt: '08/01/2023' },
+    { id: 2, value: 9, assignedAt: '04/01/2023' },
+    { id: 3, value: 10, assignedAt: '09/01/2023' },
+  ];
+
+  const result: SelectedClass = {
+    teacherName: 'Teacher 1',
+    grades,
+  };
+
+  return Promise.resolve(result);
+}
