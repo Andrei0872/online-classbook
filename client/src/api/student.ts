87dfdb51688c4209feac8b5f7464b0a7fc3f5ea4
Andrei Gatej <andreigtj01@gmail.com>
2023-01-08 23:13:10 +0200

    feat(student): mock api when fetching classes

--- client/src/api/student.ts
+++ client/src/api/student.ts
@@ -1,3 +1,17 @@
-export const fetchClasses = () => {
-
+export interface SchoolClass {
+  id: number;
+  subjectName: string;
+  studentsCount: number;
 }
+
+export const fetchClasses = (): Promise<SchoolClass[]> => {
+  const classes = [
+    { id: 1, subjectName: 'Subject1', studentsCount: 10 },
+    { id: 2, subjectName: 'Subject2', studentsCount: 20 },
+    { id: 3, subjectName: 'Subject3', studentsCount: 30 },
+    { id: 4, subjectName: 'Subject4', studentsCount: 40 },
+    { id: 5, subjectName: 'Subject5', studentsCount: 50 },
+  ];
+
+  return Promise.resolve(classes);
+};
