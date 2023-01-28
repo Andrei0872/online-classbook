7a71839b4cb1f483156843bb5a03c680fcf740b5
Andrei Gatej <andreigtj01@gmail.com>
2023-01-28 20:18:37 +0200

    feat(student): filter courses by name on the client

--- client/src/components/SearchBar/SearchBar.tsx
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -1,10 +1,11 @@
 interface SearchBarProps {
   className?: string;
+  inputEvent?: (word: string) => void;
 }
 
 function SearchBar (props: SearchBarProps) {
   return (
-    <input type="text" placeholder="Search..." className={props.className} />
+    <input type="text" placeholder="Search..." className={props.className} onInput={ev => props.inputEvent?.((ev.target as any).value)} />
   )
 }
 
--- client/src/pages/Student/Student.tsx
+++ client/src/pages/Student/Student.tsx
@@ -4,7 +4,7 @@ import SearchBar from '../../components/SearchBar/SearchBar'
 import SubjectCard from '../../components/Subject/SubjectCard'
 import { useAppSelector, useAppDispatch } from '../../utils/hooks/store';
 import { setClasses } from '../../store/slices/class.slice';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchClasses, SchoolClass } from '../../api/student';
 import { useNavigate } from 'react-router-dom'
 
@@ -12,6 +12,8 @@ function Student() {
   const classesList = useAppSelector(state => state.classes.list);
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+
+  const [nameFilter, setNameFilter] = useState('');
 
   useEffect(() => {
     if (classesList) {
@@ -26,16 +28,22 @@ function Student() {
     navigate(`student/class/${cls.id}`);
   }
 
+  const onInput = (className: string) => {
+    setNameFilter(className);
+  }
+
+  const filteredClasses = classesList?.filter(c => c.subjectName.toLowerCase().includes(nameFilter.toLowerCase()));
+
   return (
     <>
       <Header />
       <main className='student'>
-        <SearchBar className='student-search' />
+        <SearchBar inputEvent={onInput} className='student-search' />
         {
-          !classesList ? null : (
+          !filteredClasses ? null : (
             <ul className="subject-list">
               {
-                classesList.map(c => (
+                filteredClasses.map(c => (
                   <li onClick={ev => onClassClicked(c)} key={c.id} className="subject-list__item">
                     <SubjectCard subjectName={`${c.subjectName} - ${c.teacherName}`} studentsCount={c.studentsCount} />
                   </li>
